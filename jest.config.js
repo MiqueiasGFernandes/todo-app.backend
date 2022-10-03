e9be8b393a3a0d907d28b565fd33eab243d94693
Miqueias Fernandes <miqueias.fernandes@bitfy.app>
2022-10-03 14:52:29 -0300

    tests(config): mapped module directories, transformer and setup files

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,16 @@ module.exports = {
   collectCoverageFrom: [
     '<rootDir>/src/data/use-cases/*.ts',
   ],
+  transform: {
+    '^.+\\.ts?$': 'ts-jest',
+  },
+  setupFiles: [
+    '<rootDir>/src/tests/setup/LoadReflection.setup.ts',
+  ],
+  moduleNameMapper: {
+    '@domain/(.*)': '<rootDir>/src/domain/$1',
+    '@data/(.*)': '<rootDir>/src/data/$1',
+    '@infra/(.*)': '<rootDir>/src/infra/$1',
+    '@tests/(.*)': '<rootDir>/src/tests/$1',
+  },
 };
