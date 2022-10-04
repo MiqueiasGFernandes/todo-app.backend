04f06becc1674d72baa57c3feb736ad67943dd65
Miqueias Fernandes <miqueias.fernandes@bitfy.app>
2022-10-04 11:40:13 -0300

    refactor(di): remove useless methods from MainContainer

--- src/data/di/MainContainer.ts
+++ src/data/di/MainContainer.ts
@@ -1,14 +1,7 @@
 import { container, InjectionToken } from 'tsyringe'
-import { constructor } from 'tsyringe/dist/typings/types'
 
 export default abstract class MainContainer {
   private static testDependenciesTokens: InjectionToken[] = []
-
-  static createTestDependency<T>(token: InjectionToken<T>, provider: unknown): void {
-    MainContainer.testDependenciesTokens.push(token)
-
-    container.register<T>(token, provider as constructor<any>)
-  }
 
   static resolve<T>(token: InjectionToken<T>): T {
     return container.resolve<T>(token)
