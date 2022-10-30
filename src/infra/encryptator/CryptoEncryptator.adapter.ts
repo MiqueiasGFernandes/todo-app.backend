aaf0d2f8b8090ef8566324f04b4afee03baf70e8
Miqueias Fernandes <miqueias.fernandes@bitfy.app>
2022-10-30 01:26:08 -0300

    fix(crypt): ensure adapter is correctly injected as dependency

--- src/infra/encryptator/CryptoEncryptator.adapter.ts
+++ src/infra/encryptator/CryptoEncryptator.adapter.ts
@@ -1,7 +1,9 @@
 import { IEncryptatorProtocol } from '@data/protocols/encryptator/Encryptator.protocol';
 
 import CryptoJS from 'crypto-js';
+import { injectable } from 'tsyringe';
 
+@injectable()
 export default class CryptoEncryptatorAdapter implements IEncryptatorProtocol {
   async crypt(text: string): Promise<string> {
     return Promise.resolve<string>(CryptoJS.SHA512(text).toString(CryptoJS.enc.Base64))
