c985daab1e09cacd3f489c09d92fe0db108459b6
Miqueias Fernandes <miqueias.fernandes@bitfy.app>
2022-11-03 20:20:26 -0300

    fix(typo): change from "findOne" to "findOndOrFail"

--- src/data/repositories/User.repository.ts
+++ src/data/repositories/User.repository.ts
@@ -3,7 +3,7 @@ import User from '@domain/models/User.model';
 export default interface IUserRepository {
   add(data: User): Promise<User>
   delete(id: string): Promise<string>
-  findOne(id: string): Promise<User | undefined | null>
+  findOneOrFail(id: string): Promise<User>
   update(id: string, data: User): Promise<string>
   findByEmailAndPassword(email: string, password: string): Promise<User | undefined>
   countByEmail(email: string): Promise<number>
