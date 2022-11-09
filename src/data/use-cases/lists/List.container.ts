0ce10fe74ca9254194fda4ab2d47c1a122799c2f
Miqueias Fernandes <miqueias.fernandes@bitfy.app>
2022-11-09 11:11:03 -0300

    chore(lists): created DI containers

---
+++ src/data/use-cases/lists/List.container.ts
@@ -0,0 +1,7 @@
+import MainContainer from '@data/di/MainContainer';
+
+export default class ListContainer extends MainContainer {
+  static inject(): void {
+
+  }
+}
---
+++ src/presentation/controllers/ListControllers.container.ts
@@ -0,0 +1,7 @@
+import MainContainer from '@data/di/MainContainer';
+
+export default class ListControllersContainer extends MainContainer {
+  static inject(): void {
+
+  }
+}
